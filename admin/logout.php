c917b647eb7280c91c377fdb71f0963d57d636e7
fkimuu <fkimuu@gmail.com>
2019-03-25 15:00:16 +0300

    changing logout link to an icon

--- admin/logout.php
+++ admin/logout.php
@@ -1,9 +1,29 @@
 <?php
 
 session_start();
-
+unset($_SESSION);
 session_destroy();
+session_write_close();
 
 header('Location:index.php');
+exit;
 
  ?>
+
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>CMS Blog</title>
+     <link rel="stylesheet" href="../css/style.css" />
+   </head>
+   <body>
+    <div style="float:right">
+      <form align="right" name="logout" action="logout.php" method="post">
+        <label class="logoutLblPos">
+        <input type="submit" name="submit" value="Logout">
+        </label>
+      </form>
+    </div>
+
+   </body>
+ </html>
--- demo.php
+++ demo.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <title>CMS Blog</title>
     <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
   <body>
     <div class="container">
@@ -13,7 +16,10 @@
         <ol>
           <li> <a href="admin/add.php">Add article</a></li>
           <li> <a href="admin/delete.php">Delete article</a></li>
-          <li><a href="admin/logout.php">Logout</a></li>
+          <a href="admin/logout.php" class="btn btn-info btn-lg">
+            <span class="glyphicon glyphicon-log-out"></span>Logout
+          </a>
+
         </ol>
     </div>
   </body>
